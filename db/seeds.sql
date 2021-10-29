858d08711d396b029cea21fc24c86829d16fa14e
Joel Afamefune <jafamefune@gmail.com>
2021-10-28 20:28:41 -0500

    set up database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,27 @@
-INSERT INTO movies (movie_name)
-VALUES ("Lion King"),
-       ("The Godfather"),
-       ("West Side Story"),
-       ("Parasite"),
-       ("The Wizard of Oz");
+INSERT INTO department (name)
+VALUES ("Department of Defense"),
+("Department of Labor"),
+("Department of Commerce"),
+("Department of Energy"),
+("Department of Education"),
+("Department of Treasury"),
+("Department of Transportation");
 
-INSERT INTO reviews (movie_id, review)
-VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
-       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
-       (1, "Scar is the lion everyone loves to hate"),
-       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
-       (5, "The tin man gave a metallic, hollow performance"),
-       (1, "Hakuna matata"),
-       (5, "Those flying monkeys are nightmare fuel!");
-       
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Secretary of Defense", 200000, 1),
+("Secretary of Labor", 200000, 2),
+("Secretary of Commerce", 200000, 3),
+("Secretary of Energy", 200000, 4),
+("Secretary of Education", 200000, 5),
+("Secretary of Treasury", 200000, 6),
+("Secretary of Transportation", 200000, 7),
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES VALUES ("Jane", "Doe", 1, 3),
+("Joel", "Afamefune", 2, 3),
+("Roy", "Woods", 3, 3),
+("Amir", "Obre", 4, 3),
+("Rockie", "Fresh", 5, 3),
+("Isaiah", "Rashad", 6, 3),
+("Marcus", "Garvey", 7, 3),
+("Mick", "Jenkins", 8, 3);
